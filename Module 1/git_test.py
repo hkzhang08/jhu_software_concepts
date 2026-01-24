abaf757c7859321833d12a6d8ebb4966b751dc21
Helen Zhang <zhang8@Mac.lan>
2026-01-23 21:25:05 -0500

    Changing folder name to module_1 like canvas instructions said

--- Module 1/git_test.py
+++
@@ -1,6 +0,0 @@
-# test for git to see if its matching
-print("Hello World")
-
-print("This is Helen")
-
-print("Another test")
---
+++ Module_1/git_test.py
@@ -0,0 +1,13 @@
+"""
+Class:         EP 605 2256 81 - Spring 2026
+Assignment:    Module 1 - Personal Website
+Due Date:      January 25th by 11:59AM
+Name:          Helen Zhang
+Email:         hzhan308@jh.edu
+"""
+
+
+
+# Skills: Flask, HTML, Searching Methods, Data Cleaning, JSON Data Object Storage
+
+
