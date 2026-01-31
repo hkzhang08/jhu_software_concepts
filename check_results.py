a28ecdd3765fdfcec0b3a235637f0ae2f4bec1af
Helen Zhang <zhang8@Mac.lan>
2026-01-30 23:43:09 -0500

    update to module_2 folder

--- check_results.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("gradcafe_results.jsonl") as f:
-    records = [json.loads(line) for line in f]
-
-print("Total records:", len(records))
-print("First record:")
-print(records[0])
