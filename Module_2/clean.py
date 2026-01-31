4effdc2bad042ccf7761bdbbcd21c452716892de
Helen Zhang <zhang8@Mac-2.lan>
2026-01-31 09:52:54 -0500

    official data from scraper

---
+++ Module_2/clean.py
@@ -0,0 +1,4 @@
+
+
+# D.	Clean the data using an LLM
+# E.	Structure the data as a clean, formatted JSON data object.
--- Module_2/scrape.py
+++ Module_2/scrape.py
@@ -216,6 +216,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# D.	Clean the data using an LLM
-# E.	Structure the data as a clean, formatted JSON data object.
